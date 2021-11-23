1fb562c220fc8ec87a1666cd00b41c88984259b2
Walter Wang <wzc314159@hotmail.com>
2021-11-23 23:21:43 +0800

    init component

---
+++ src/components/Stack.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+export default function Stack({
+  children,
+  spacing = 2,
+  direction = 'row',
+  wrap = false,
+}) {
+  const style = {
+    display: 'flex',
+    gap: `${spacing * 0.25}rem`,
+    flexWrap: wrap ? 'wrap' : 'nowrap',
+    flexDirection: direction,
+  };
+  return <div style={style}>{children} </div>;
+}
+
+Stack.propTypes = {
+  spacing: PropTypes.string,
+  wrap: PropTypes.bool,
+  direction: PropTypes.oneOf(['row', 'column']),
+};
